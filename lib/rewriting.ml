4bef6a0042d7b0eec2247fdc3d059dfd9f7410f0
Léo LEESCO <leo.leesco@gmail.com>
2025-04-25 21:10:40 -0700

    rewriting

---
+++ lib/rewriting.ml
@@ -0,0 +1,16 @@
+open Matching
+open Base
+
+exception Normal_form_reached
+
+let rec rewrite trs t =
+  match trs with
+  | [] -> raise Normal_form_reached
+  | (l, r) :: trs -> ( try match_term l t &@@ r with Unify _ -> rewrite trs t)
+
+let rec norm trs t =
+  match t with
+  | V x -> V x
+  | T (f, ts) -> (
+      let u = T (f, List.map (norm trs) ts) in
+      try norm trs (rewrite trs u) with Normal_form_reached -> u)
