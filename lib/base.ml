15c0c6a14e5acaf59ec9ed120e97e37a0511b029
Léo LEESCO <leo.leesco@gmail.com>
2025-04-30 16:08:00 -0700

    basics orders
    TODO : KB order

--- lib/base.ml
+++ lib/base.ml
@@ -7,22 +7,22 @@ type term =
 type subst = (vname * term) list
 (* {("x",1) -> ("t",0) ; ("y",2) -> T ("f", [T("a",[]),V("y",1)])} *)
 
-(* test if x in Dom(sub) *)
+(** indom (p.79) * test if x in Dom(sub) *)
 let ( &? ) x sub = List.exists (fun (y, _) -> x = y) sub
 
-(* apply substitution to single variable *)
+(** app (p.79) * apply substitution to single variable *)
 let rec ( &@ ) sub x =
   match sub with
   | (y, t) :: _ when x = y -> t
   | _ :: dom -> dom &@ x
   | [] -> raise Not_found
 
-(* apply substitution to a term *)
+(** lift (p.79) * apply substitution to a term *)
 let rec ( &@@ ) sub = function
   | V x -> ( try sub &@ x with Not_found -> V x)
   | T (f, t) -> T (f, List.map (fun term -> sub &@@ term) t)
 
-(* test if x in Var(t) *)
+(** occurs (fig 4.5) * test if x in Var(t) *)
 let rec ( &&? ) x = function
   | V y -> x = y
   | T (_, t) -> List.exists (fun u -> x &&? u) t
--- lib/orders.ml
+++ lib/orders.ml
@@ -1,3 +1,62 @@
 open Base
 
-type lt = term * term
+type ord = LT | EQ | GT
+type 'a order = 'a * 'a -> ord
+
+let rec lex (ord : 'a order) = function
+  | l, m when List.length l <> List.length m ->
+      if List.length l > List.length m then GT else LT
+  | a :: l, b :: m -> if ord (a, b) = EQ then lex ord (l, m) else ord (a, b)
+  | [], [] -> EQ
+  | [], _ | _, [] ->
+      failwith
+        "element-wise comparison should not be performed on different length \
+         lists"
+
+let rec lex_path_order (ord : string order) = function
+  | s, (V x as t) -> if s = t then EQ else if x &&? s then GT (* LPO1 *) else LT
+  | V _, T _ -> LT
+  | (T (f, sl) as s), (T (g, tl) as t) ->
+      (* LPO2 *)
+      if List.for_all (fun si -> lex_path_order ord (si, t) = LT) sl then
+        match ord (f, g) with
+        | GT ->
+            if List.for_all (fun ti -> lex_path_order ord (s, ti) = GT) tl then
+              GT (* LPO2b *)
+            else LT
+        | EQ ->
+            if List.for_all (fun ti -> lex_path_order ord (s, ti) = GT) tl then
+              lex (lex_path_order ord) (sl, tl) (* LPO2c *)
+            else LT
+        | LT -> LT
+      else GT (* LPO2a *)
+
+let rec rec_path_order
+    (status : string -> term order -> term list * term list -> ord)
+    (ord : string order) = function
+  | s, (V x as t) -> if s = t then EQ else if x &&? s then GT (* LPO1 *) else LT
+  | V _, T _ -> LT
+  | (T (f, sl) as s), (T (g, tl) as t) ->
+      if List.for_all (fun si -> rec_path_order status ord (si, t) = LT) sl then
+        match ord (f, g) with
+        | GT ->
+            if
+              List.for_all (fun ti -> rec_path_order status ord (s, ti) = GT) tl
+            then GT
+            else LT
+        | EQ ->
+            if
+              List.for_all (fun ti -> rec_path_order status ord (s, ti) = GT) tl
+            then (status f) (rec_path_order status ord) (sl, tl)
+            else LT
+        | LT -> LT
+      else GT
+
+(** returns a positive function over terms, induced by a weight function over
+    variables and function symbols
+
+    it should be admissible when used with an order (for the
+    {!knuth_bendix_order}) *)
+let term_weight (w : vname -> float) = failwith "todo"
+
+let rec knuth_bendix_order (ord : term order) (w : term -> float) = ()
