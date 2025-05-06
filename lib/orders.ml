2f1dca18cd284506e07ad00cf963b4e0f8b15e0a
Léo LEESCO <leo.leesco@gmail.com>
2025-05-06 11:09:12 -0700

    abide by stdlib signature for comparison operators
    total orders are described as 'a -> 'a -> int
    partial orders as 'a -> 'a -> int option

--- lib/orders.ml
+++ lib/orders.ml
@@ -1,62 +1,67 @@
 open Base
 
-type ord = LT | EQ | GT
-type 'a order = 'a * 'a -> ord
+(** [lex l m] returns the sign of "[l - m]", i.e.
 
-let rec lex (ord : 'a order) = function
-  | l, m when List.length l <> List.length m ->
-      if List.length l > List.length m then GT else LT
-  | a :: l, b :: m -> if ord (a, b) = EQ then lex ord (l, m) else ord (a, b)
-  | [], [] -> EQ
+    [l<m ->] negative
+
+    [l=m ->] 0
+
+    [l>m ->] positive *)
+let rec lex (ord : 'a -> 'a -> int) = function
+  | l, m when List.length l <> List.length m -> List.length l - List.length m
+  | a :: l, b :: m -> if ord a b = 0 then lex ord (l, m) else ord a b
+  | [], [] -> 0
   | [], _ | _, [] ->
-      failwith
-        "element-wise comparison should not be performed on different length \
-         lists"
+      failwith "element-wise comparison should have already be handled"
 
-let rec lex_path_order (ord : string order) = function
-  | s, (V x as t) -> if s = t then EQ else if x &&? s then GT (* LPO1 *) else LT
-  | V _, T _ -> LT
+let rec lex_path_order (ord : string -> string -> int) = function
+  | s, (V x as t) -> if s = t then 0 else if x &&? s then 1 (* LPO1 *) else -1
+  | V _, T _ -> -1
   | (T (f, sl) as s), (T (g, tl) as t) ->
       (* LPO2 *)
-      if List.for_all (fun si -> lex_path_order ord (si, t) = LT) sl then
-        match ord (f, g) with
-        | GT ->
-            if List.for_all (fun ti -> lex_path_order ord (s, ti) = GT) tl then
-              GT (* LPO2b *)
-            else LT
-        | EQ ->
-            if List.for_all (fun ti -> lex_path_order ord (s, ti) = GT) tl then
-              lex (lex_path_order ord) (sl, tl) (* LPO2c *)
-            else LT
-        | LT -> LT
-      else GT (* LPO2a *)
+      if List.for_all (fun si -> lex_path_order ord (si, t) < 0) sl then
+        let sg = ord f g in
+        if sg > 0 then
+          if List.for_all (fun ti -> lex_path_order ord (s, ti) > 0) tl then 1
+            (* LPO2b *)
+          else -1
+        else if sg = 0 then
+          if List.for_all (fun ti -> lex_path_order ord (s, ti) > 0) tl then
+            lex (fun s t -> lex_path_order ord (s, t)) (sl, tl) (* LPO2c *)
+          else -1
+        else -1
+      else 1 (* LPO2a *)
 
+(** generalizes [lex_path_order] and [multiset_path_order] using a dynamic
+    [status] parameter
+
+    [status f] is the order used for the term [f(s1…sn)], which means it can be
+    dynamically chosen based on the function used*)
 let rec rec_path_order
-    (status : string -> term order -> term list * term list -> ord)
-    (ord : string order) = function
-  | s, (V x as t) -> if s = t then EQ else if x &&? s then GT (* LPO1 *) else LT
-  | V _, T _ -> LT
+    ~(status : string -> (term -> term -> int) -> term list * term list -> int)
+    (ord : string -> string -> int) = function
+  | s, (V x as t) -> if s = t then 0 else if x &&? s then 1 (* LPO1 *) else -1
+  | V _, T _ -> -1
   | (T (f, sl) as s), (T (g, tl) as t) ->
-      if List.for_all (fun si -> rec_path_order status ord (si, t) = LT) sl then
-        match ord (f, g) with
-        | GT ->
-            if
-              List.for_all (fun ti -> rec_path_order status ord (s, ti) = GT) tl
-            then GT
-            else LT
-        | EQ ->
-            if
-              List.for_all (fun ti -> rec_path_order status ord (s, ti) = GT) tl
-            then (status f) (rec_path_order status ord) (sl, tl)
-            else LT
-        | LT -> LT
-      else GT
+      if List.for_all (fun si -> rec_path_order ~status ord (si, t) < 0) sl then
+        let sg = ord f g in
+        if sg > 0 then
+          if List.for_all (fun ti -> rec_path_order ~status ord (s, ti) > 0) tl
+          then 1
+          else -1
+        else if sg = 0 then
+          if List.for_all (fun ti -> rec_path_order ~status ord (s, ti) > 0) tl
+          then (status f) (fun s t -> rec_path_order ~status ord (s, t)) (sl, tl)
+          else -1
+        else -1
+      else 1
 
 (** returns a positive function over terms, induced by a weight function over
     variables and function symbols
 
     it should be admissible when used with an order (for the
     {!knuth_bendix_order}) *)
-let term_weight (w : vname -> float) = failwith "todo"
+let term_weight (w : vname -> float) = failwith "TODO"
 
-let rec knuth_bendix_order (ord : term order) (w : term -> float) = ()
+let rec knuth_bendix_order (ord : term -> term -> int) (w : term -> float) =
+  failwith "TODO"
