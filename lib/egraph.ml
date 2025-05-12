0bfaa0322dd851027796dbc607e156c920400e85
Léo LEESCO <leo.leesco@gmail.com>
2025-05-12 16:21:58 -0700

    start union find…

--- lib/egraph.ml
+++ lib/egraph.ml
@@ -1,3 +1,16 @@
 module UnionFind = struct
-  ()
+  type 'a node = { data : 'a; parent : 'a node option; depth : int }
+  (** depth should be the max number of children, 0 when a leaf *)
+
+  let rec find n =
+    match n.parent with
+    | None -> n
+    | Some parent -> { n with parent = Some (find parent); depth = 1 }
+
+  let union n m =
+    if n.depth = m.depth then
+      { m with parent = Some { n with depth = n.depth + 1 } }
+    else if n.depth < m.depth then
+      { n with parent = Some { m with depth = m.depth } }
+    else { m with parent = Some { n with depth = n.depth } }
 end
