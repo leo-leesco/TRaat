cdc6435f4179bdb282f5eb64151a1efe279a3e49
Léo LEESCO <leo.leesco@gmail.com>
2025-05-13 16:07:06 -0700

    start egraph

--- lib/egraph.ml
+++ lib/egraph.ml
@@ -4,31 +4,39 @@ module UnionFind = struct
 
       (not reduced by path compression, so it is actually worst case) *)
 
-  type 'a graph = 'a node Array.t
+  type 'a graph = 'a node Dynarray.t
+
+  (* those serve to mimic the interface of Array *)
+  let ( .!() ) = Dynarray.get
+  let ( .!()<- ) = Dynarray.set
 
   let make data_array =
-    Array.mapi (fun idx data -> { data; parent = idx; depth = 0 }) data_array
+    Dynarray.mapi (fun idx data -> { data; parent = idx; depth = 0 }) data_array
+
+  let add (graph : 'a graph) data =
+    Dynarray.add_last graph { data; parent = Dynarray.length graph; depth = 0 }
 
   (** returns the index of the ancestor
 
       performs path compression *)
-  let rec find graph node_idx =
-    if not (0 <= node_idx && node_idx <= Array.length graph) then
+  let rec find (graph : 'a graph) node_idx =
+    if not (0 <= node_idx && node_idx <= Dynarray.length graph) then
       raise (Invalid_argument "Tried to access an out-of-bounds node")
-    else if graph.(node_idx).parent <> node_idx then (
-      graph.(node_idx).parent <- find graph node_idx;
-      graph.(node_idx).parent)
+    else if graph.!(node_idx).parent <> node_idx then (
+      graph.!(node_idx).parent <- find graph node_idx;
+      graph.!(node_idx).parent)
     else node_idx
 
   let union graph n m =
     let n = find graph n in
     let m = find graph m in
 
-    if graph.(n).depth > graph.(m).depth then graph.(m).parent <- n
-    else if graph.(n).depth < graph.(m).depth then graph.(n).parent <- m
+    if graph.!(n).depth > graph.!(m).depth then graph.!(m).parent <- n
+    else if graph.!(n).depth < graph.!(m).depth then graph.!(n).parent <- m
     else (
-      graph.(m).parent <- n;
-      graph.(n).depth <- graph.(n).depth + 1)
+      graph.!(m).parent <- n;
+      graph.!(n).depth <- graph.!(n).depth + 1)
 end
 
-type 'a enode = { data : 'a UnionFind.node; parent : 'a enode option }
+type 'a enode = { data : 'a; children : int list }
+type 'a egraph = 'a enode UnionFind.graph
