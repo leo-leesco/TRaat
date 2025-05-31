7c5fb9b45a2aa5cfe8df729f525de8f664d81d63
Léo LEESCO <leo.leesco@gmail.com>
2025-05-30 17:18:01 -0700

    test unionFind

--- test/test_unionFind.ml
+++ test/test_unionFind.ml
@@ -2,4 +2,8 @@ open UnionFind
 
 let () =
   let classes = create 5 in
-  print_endline (of_string classes)
+  print_endline (of_string classes);
+  let set = create 6 in
+  concat classes set;
+  print_endline (of_string classes);
+  assert (classes = create 11)
