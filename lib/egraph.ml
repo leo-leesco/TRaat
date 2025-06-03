2d34674806ffde625b4b200221f32273f8be743d
Léo LEESCO <leo.leesco@gmail.com>
2025-06-03 10:53:15 -0700

    move to UF+H only
    rely on H to check for already existing keys in UF

--- lib/egraph.ml
+++ lib/egraph.ml
@@ -1,31 +1,33 @@
 type 'a enode = { data : 'a; mutable children : UnionFind.id list }
 
 type 'a graph = {
-  unionfind : UnionFind.t;
-  store : 'a Dynarray.t;
+  unionfind : 'a UnionFind.t;
   hashcons : ('a, UnionFind.id) Hashtbl.t;
 }
 (** [store] contains the data and is immutable, so classes are stored in
     [unionfind] *)
 
 let of_list l =
-  {
-    unionfind = UnionFind.create (List.length l);
-    store = Dynarray.of_list l;
-    hashcons =
-      Hashtbl.of_seq (List.mapi (fun idx data -> (data, idx)) l |> List.to_seq);
-  }
+  let hashcons = Hashtbl.create (List.length l) in
+  List.iteri
+    (fun idx data ->
+      if Hashtbl.mem hashcons data then invalid_arg "duplicate entry"
+      else Hashtbl.add hashcons data idx)
+    l;
+  { unionfind = UnionFind.create (Dynarray.of_list l); hashcons }
 
-let add (graph : 'a graph) (idx : UnionFind.id) (data : 'a) =
-  UnionFind.add graph.unionfind idx;
-  Dynarray.add_last graph.store data
+let add (graph : 'a graph) (data : 'a) =
+  try (
+    H
+  )
+  let idx = UnionFind.add graph.unionfind data in
+  Hashtbl.add graph.hashcons data idx
 
 (** [concat graph1 graph2] mutates [graph1] in-place and adds [graph2] at the
     end, preserving the parents of [graph2] logically
 
     in particular, the classes are still disjoint after the [concat]enation *)
 let concat (graph1 : 'a graph) (graph2 : 'a graph) =
-  UnionFind.concat graph1.unionfind graph2.unionfind;
-  Dynarray.append graph1.store graph2.store
+  UnionFind.concat graph1.unionfind graph2.unionfind
 
 type 'a egraph = 'a enode graph
